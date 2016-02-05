d233ab0c7495bd1ddd115f065b29d115c35643ea
t4kq1995 <vlad13zp@mail.ru>
2016-02-05 11:48:45 +0200

    Change database to PosgreSQL

--- wall_posts/wall_posts/settings.py
+++ wall_posts/wall_posts/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wall_posts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wall_posts',
+        'USER': 'root',
+        'PASSWORD': 'pro100vlad',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
