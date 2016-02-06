134fa359e83f80b0ca7cc95b55839e575248a131
t4kq1995 <vlad13zp@mail.ru>
2016-02-06 16:02:14 +0200

    Message page with access denied

--- wall_posts/wall/urls.py
+++ wall_posts/wall/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url, include
 from wall import views
 
 urlpatterns = [
-    url(r'^$', views.LoginView.as_view(), name='check'),
+    url(r'^$', views.LoginView.as_view(), name='login'),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/profile/$', views.MessageView.as_view(), name='index'),
 ]
--- wall_posts/wall/views.py
+++ wall_posts/wall/views.py
@@ -1,9 +1,21 @@
 from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 
 class LoginView(TemplateView):
     template_name = 'index.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(reverse('index'))
+
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+
+class MessageView(TemplateView):
+    template_name = 'message.html'
+
     def get_context_data(self, **kwargs):
-        context = super(LoginView, self).get_context_data(**kwargs)
+        context = super(MessageView, self).get_context_data(**kwargs)
         return context
